61096237957533555707c9786de15b2b5bc73996
Joni Kukko <requestnetwork@kukko.email>
2018-01-22 13:41:13 +0200

    Created common exception for RequestNetwork

---
+++ src/main/java/network/request/services/RequestNetworkException.java
@@ -0,0 +1,18 @@
+package network.request.services;
+
+public class RequestNetworkException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public RequestNetworkException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RequestNetworkException(String message) {
+        super(message);
+    }
+
+    public RequestNetworkException(Throwable cause) {
+        super(cause);
+    }
+}
